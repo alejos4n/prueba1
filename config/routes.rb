56dccc9cc20f6c1bca3a670b619b5f0b1dbde233
alejandro <alejosanchezlopera@gmail.com>
2018-10-08 00:43:05 -0500

    controlador manual

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  resources :todos
+  get 'todo/index'
+
+  get 'todo/show'
+
+  get 'todo/create'
+
+  get 'todo/edit'
+
+  get 'todo/update'
+
+  get 'todo/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
