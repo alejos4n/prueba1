5bfc5c8009aae0e7ffcbf51855f1e51b4825306d
alejandro <alejosanchezlopera@gmail.com>
2018-10-11 01:33:49 -0500

    agregando estilo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :todos
+  resources :comments
   get 'todo/index'
 
   get 'todo/show'
@@ -12,7 +13,5 @@ Rails.application.routes.draw do
 
   get 'todo/destroy'
 
-  get '/comment' => 'comment#index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
